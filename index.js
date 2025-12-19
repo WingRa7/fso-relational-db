ccccea9cf5d9fc21302fcbe5e815ae01f59bc90c
Tom Raine <tomgraine@gmail.com>
2025-12-19 17:46:29 +0800

    Exercise 13.4 complete

--- index.js
+++ index.js
@@ -39,9 +39,43 @@ Blog.init(
   }
 );
 
+Blog.sync();
+
 app.get("/api/blogs", async (req, res) => {
   const blogs = await Blog.findAll();
+
+  console.log(JSON.stringify(blogs, null, 2));
   res.json(blogs);
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    console.log(blog.toJSON());
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    console.log(blog.toJSON());
+    return res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = process.env.PORT || 3001;
