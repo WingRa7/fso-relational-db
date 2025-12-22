c357c0997cee5d72632184aa734aca854a106e38
Tom Raine <tomgraine@gmail.com>
2025-12-22 16:58:59 +0800

    Exercise 13.16 complete

---
+++ controllers/authors.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const { Blog, User } = require("../models");
+const { sequelize } = require("../util/db");
+
+router.get("/", async (req, res) => {
+  // author, articles (count of blogs), likes (total likes)
+  const blogs = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn("SUM", sequelize.col("likes")), "likes"],
+      [sequelize.fn("COUNT", sequelize.col("id")), "articles"],
+    ],
+    group: "author",
+    order: [["likes", "DESC"]],
+  });
+
+  console.log(JSON.stringify(blogs, null, 2));
+  res.json(blogs);
+});
+
+module.exports = router;
